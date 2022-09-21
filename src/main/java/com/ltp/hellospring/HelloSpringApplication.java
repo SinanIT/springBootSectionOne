eec9182d07bb8ade45685b7d3a0fff574cdf4255
SinanIT <sinandegerli@gmail.com>
2022-09-21 08:46:57 -0400

    Third Commit

--- src/main/java/com/ltp/hellospring/HelloSpringApplication.java
+++ src/main/java/com/ltp/hellospring/HelloSpringApplication.java
@@ -2,9 +2,9 @@ package com.ltp.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+
+@SpringBootApplication
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
